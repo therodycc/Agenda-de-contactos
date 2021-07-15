ec843d1454f9c1bf94550c5e3dd08541cc6a6fdf
therodycc <therodycc@gmail.com>
2021-07-15 10:20:31 -0400

    nuevas modificaciones

--- main.js
+++ main.js
@@ -1,67 +1,126 @@
 const app = new Vue({
-    el: '#app',
+  el: "#app",
 
-    data: {
-        contactos:[],
-		txtnombre:'',
-		txttelefono:'',
-        txtcorreo:'',
-        txtbuscar: '',
-      
-    },
-    methods:{
-        LimpiarCampos(){
-            this.txtnombre = '';
-			this.txttelefono = '';
-			this.txtcorreo = '';
-        },
-        Agregar(){
-
-            if (this.txtnombre === '' || this.txttelefono === '' || this.txtcorreo === '') {
-                alert("Por favor llene todos los campos")
-            }else{
-                this.contactos.push
-                ({
-                    nombre: this.txtnombre,
-                    telefono: this.txttelefono,
-                    correo: this.txtcorreo,
-                })	
-                this.LimpiarCampos()
-                localStorage.setItem('Contactos-agenda',JSON.stringify(this.contactos));
-                }
-            },
-
-        Editar(index){
-            if (this.txtnombre === '' || this.txttelefono === '' || this.txtcorreo === '') {
-                alert("Digite todos los campos. para actualizar")
-            }else{
-                this.contactos[index].nombre = this.txtnombre;
-                this.contactos[index].telefono = this.txttelefono;
-                this.contactos[index].correo = this.txtcorreo;
-              
-            }
-                this.LimpiarCampos();
-                localStorage.setItem('Contactos-agenda',JSON.stringify(this.contactos));
-            },
-        Eliminar(contacto){
-                let index = this.contactos.indexOf(contacto)
-                this.contactos.splice(index,1)
-                localStorage.setItem('Contactos-agenda',JSON.stringify(this.contactos));localStorage.setItem('Contactos', JSON.stringify(this.contactos));
-        },
-      
-    
+  data: {
+    contacts: [],
+    txtName: "",
+    txtPhone: "",
+    txtEmail: "",
+    txtSearch: "",
+  },
+  methods: {
+    clearInputs() {
+      this.txtName = "";
+      this.txtPhone = "";
+      this.txtEmail = "";
     },
 
+    addContact() {
+      if (this.txtName === "" || this.txtPhone === "" || this.txtEmail === "") {
+        Swal.fire({
+          icon: "info",
+          title: "Por favor",
+          text: "Debe llenar todos los campos.",
+          timer: 3000,
+        });
+      } else {
+        let contact = {
+          name: this.txtName,
+          phone: this.txtPhone,
+          email: this.txtEmail,
+        };
 
-    created() {
-		let Contenderdatos = JSON.parse(localStorage.getItem('Contactos-agenda'));
-		console.log(Contenderdatos);
+        this.contacts.push(contact);
 
-		if (Contenderdatos === null) {
-			this.contactos = [];
-		} else {
-			this.contactos = Contenderdatos;
-		}
-	},
-})
+        localStorage.setItem("contacts-agenda", JSON.stringify(this.contacts));
 
+        this.clearInputs();
+        Swal.fire({
+          icon: "success",
+          title: "Guardado",
+          showConfirmButton: false,
+          timer: 1000,
+        });
+      }
+    },
+
+    editContact(index) {
+      if (this.txtName === "" || this.txtPhone === "" || this.txtEmail === "") {
+        Swal.fire({
+          icon: "info",
+          title: "Por favor",
+          text: "Debe llenar todos los campos. para actualizar",
+          timer: 3000,
+        });
+
+      } else {
+
+        this.contacts[index].name = this.txtName;
+        this.contacts[index].phone = this.txtPhone;
+        this.contacts[index].email = this.txtEmail;
+        
+      }
+      this.clearInputs();
+      localStorage.setItem("contacts-agenda", JSON.stringify(this.contacts));
+    },
+
+    deleteContact(contact) {
+      let index = this.contacts.indexOf(contact);
+
+      Swal.fire({
+        title: "¿Estas seguro?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#1D976C",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this.contacts.splice(index, 1);
+          localStorage.setItem(
+            "contacts-agenda",
+            JSON.stringify(this.contacts)
+          );
+          localStorage.setItem("contacts", JSON.stringify(this.contacts));
+          Swal.fire({
+            icon: "success",
+            title: "Eliminado",
+            showConfirmButton: false,
+            timer: 1000,
+          });
+        }
+      });
+    },
+
+    search() {
+      this.contacts.forEach((contact) => {
+        if (this.txtSearch == contact.name) {
+          console.log(contact);
+          this.contacts = [contact];
+        } else {
+          this.contacts = this.containerData[0];
+        }
+      });
+    },
+
+    sweetAlertAdded() {
+      // const { emailSweet: email } = await Swal.fire({
+      //   title: 'Nombre',
+      //   input: 'email',
+      //   inputLabel: 'Your email address',
+      //   inputPlaceholder: 'Enter your email address'
+      // })
+      
+    },
+  },
+
+  created() {
+    let containerData = JSON.parse(localStorage.getItem("contacts-agenda"));
+
+    if (containerData === null) {
+      this.contacts = [];
+    } else {
+      this.contacts = containerData;
+    }
+  },
+});
